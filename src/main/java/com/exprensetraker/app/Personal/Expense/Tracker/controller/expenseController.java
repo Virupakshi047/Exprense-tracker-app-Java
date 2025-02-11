67d0be548738b552698101e0affa8a0d84a0c3fe
Virupakshi <virupakshivijji@gmail.com>
2025-02-11 15:17:56 +0530

    Initialized spring boot and connected to MySql partially

---
+++ src/main/java/com/exprensetraker/app/Personal/Expense/Tracker/controller/expenseController.java
@@ -0,0 +1,8 @@
+package com.exprensetraker.app.Personal.Expense.Tracker.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class expenseController {
+    
+}
